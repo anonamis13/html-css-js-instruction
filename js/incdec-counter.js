c5e78c1f194758c11c0436c660e70094f7d90bbc
Trevor Osborne <trevor.osborne113@gmail.com>
2020-05-01 09:27:51 -0400

    enhanced incedc-counter project

--- js/incdec-counter.js
+++ js/incdec-counter.js
@@ -2,42 +2,21 @@ let count = 0;
 
 const inc = () => {
     count++;
-    let inpt = document.getElementById("counter");
-    inpt.style.removeProperty("color");
-    inpt.style.removeProperty("font-style");
-    inpt.style.removeProperty("font-weight");
-    if (count % 2 == 0) {
-        inpt.style.color='red';
-    }
-    if (count % 3 == 0) {
-        inpt.style.fontStyle='italic';
-    }
-    if (count % 7 == 0) {
-        inpt.style.fontWeight='bold';
-    }
-    if (count == 0) {
-        inpt.style.removeProperty("color");
-        inpt.style.removeProperty("font-style");
-        inpt.style.removeProperty("font-weight");
-    }
-    inpt.value = count;
+    display(count);
 }
 
 const dec = () => {
     count--;
+    display(count);
+}
+
+function display (count) {
     let inpt = document.getElementById("counter");
-    inpt.style.removeProperty("color");
-    inpt.style.removeProperty("font-style");
-    inpt.style.removeProperty("font-weight");
-    if (count % 2 == 0) {
-        inpt.style.color='red';
-    }
-    if (count % 3 == 0) {
-        inpt.style.fontStyle='italic';
-    }
-    if (count % 7 == 0) {
-        inpt.style.fontWeight='bold';
-    }
+    
+    inpt.style.color = (count % 2 == 0) ? "red" : "black";
+    inpt.style.fontStyle = (count % 3 == 0) ? "italic" : "normal";
+    inpt.style.fontWeight = (count % 7 == 0) ? "bold" : "normal";
+
     if (count == 0) {
         inpt.style.removeProperty("color");
         inpt.style.removeProperty("font-style");
